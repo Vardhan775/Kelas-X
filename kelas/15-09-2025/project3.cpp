aee19bfb50cc60c8ce3f4a4c082156093a1b7a83
Vardhan775 <Vardhan775@gmail.com>
2025-09-18 09:27:24 +0700

    PHP

---
+++ kelas/15-09-2025/project3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout << "Belajar Program:";
+    int a;
+    cout << "inputkan angka:";
+    cin >> a;
+    cout << a;
+    return 0;
+}
