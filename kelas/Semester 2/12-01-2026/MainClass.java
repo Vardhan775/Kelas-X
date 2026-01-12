0ef5477fdf51b43091ebf827bf32da40131de4c3
Vardhan775 <vardhanaditya031@gmail.com>
2026-01-12 09:50:11 +0700

    12-01-2026

---
+++ kelas/Semester 2/12-01-2026/MainClass.java
@@ -0,0 +1,27 @@
+public class MainClass {
+
+    public static void main(String[] ags) {
+        int angka;
+        angka = 10;
+            System.out.println(angka);
+            System.out.println("Angka yang anda inputkan: "+angka);
+        angka = 100;
+            System.out.println("Angka yang anda inputkan: "+angka);
+
+        double desimal=10.355;
+        float pecahan= 13.4555f;
+            System.out.println(desimal);
+            System.out.println("Angka yang diinput: "+pecahan);
+
+        String nama= "Jonathan";
+        char abjad= 'A';
+            System.out.println(nama);
+            System.out.println(abjad);
+
+        boolean betul= true;
+        boolean salah= false;
+            System.out.println(betul);
+            System.out.println(salah);
+    }
+
+}
