1cd63630f9528d77cd4c9605524ad5a59a5139bd
Vardhan775 <vardhanaditya031@gmail.com>
2026-04-14 14:00:58 +0700

    OverLoading

---
+++ Pak Tri/Semester 2/BelajarJavaOOP/14-04-2026/OverL.java
@@ -0,0 +1,13 @@
+public class OverL {
+
+    public static void main(String[] args) {
+        Overloading nm = new Overloading();
+            nm.perkalian(5, 4);
+            nm.perkalian(2, 3, 4);
+            nm.perkalian(2.5, 4.0);
+            nm.perkalian(2.5f, 4.9f);
+            nm.perkalian(1, 2, 3, 4);
+    }
+
+
+}
